35bfaa8e4cefba129d472ef5567685953fcfc5ae
DraganaPilipovic <52774843+DraganaPilipovic@users.noreply.github.com>
2020-02-19 00:30:50 +0100

    Update absoluteValuesSumMinization.ts

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,11 +1,13 @@
-function absoluteValuesSumMinimization(a: number[]): number {
-if (a%2 === 0) {
-return a[(a.lenght/2)]
+function absoluteValuesSumMinimization(a) {
+
+if (a.length%2 === 0) {
+  return a[a.length/2-1];
 } else {
-return a[(a.lenght-1)/2]}
+  return a[Math.floor(a.length/2)];
+}
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
